f718e70db7ac47dba2f00ff5fc7d5102a0e37d32
debbyyeh <app2984@gmail.com>
2025-08-26 19:23:28 +0800

    edit selected card UI

--- src/pages/HeroProfile.tsx
+++ src/pages/HeroProfile.tsx
@@ -2,20 +2,36 @@ import { useParams } from "react-router-dom"
 import { SubTitle } from "../components/card"
 import HeroesList from "../components/HeroesList"
 import { useHeroStore } from "../domain/heroStore"
+import { useEffect } from "react"
+import { fetchHeros } from "../util/apiUtil"
 
 export default function HeroProfile(){
     const heroesList = useHeroStore(state => state.heroesList)
-    const {id} = useParams()
+    const setHeroesData = useHeroStore(state => state.setHeroesData)
+    const {id} = useParams<{ id: string }>()
+    const currentHero = heroesList.find(hero => hero.id === id)
+
+
+    useEffect(()=>{
+        if(heroesList.length === 0){
+            fetchHeros().then(data =>{
+                setHeroesData(data)
+            })
+        }
+    },[setHeroesData, heroesList])
+
 
     return(
         <>
         <h1>Hero Profile Page</h1>
-        {heroesList.length === 0 ?<p>英雄集結中</p> : 
+        {heroesList.length === 0 ? (
+            <p>英雄集結中...</p>
+            ) : (
             <>
-                <SubTitle>點選下方按鈕幫英雄變身！</SubTitle>
+                <SubTitle>現在選擇的是 {currentHero!.name}</SubTitle>
                 <HeroesList currentHeroes={heroesList} id={id}/>
             </>
-        }
+            )}
         </>
     )
 }
